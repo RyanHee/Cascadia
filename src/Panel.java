fa29e955f89521032d011a1ca6b7e40275f64517
Josh Owoeye <owoeyejay@gmail.com>
2024-04-05 11:58:21 -0500

    DOOOOOOOOOO

--- src/Panel.java
+++ src/Panel.java
@@ -122,9 +122,7 @@ public class Panel extends JPanel implements ActionListener {
         add(cancelB);
         cancelB.setBounds(getWidth()/30, getHeight()*3/5, getWidth()/15, getHeight()/15);
 
-        add(up);
-
-        up.setBounds(0,0,50,50);
+       
         g.drawImage(dpad, 800, 600, 240, 240, null);
 
         g.drawImage(rotateImage, 1204, 500, 75, 87, null);
@@ -142,9 +140,25 @@ public class Panel extends JPanel implements ActionListener {
         //down.showButton();
         add(down);
         down.setBounds(897, 747, 50, 50);
-        //add(bp);
-        bp.setBounds(30, 80, 700, 700);
+        
         g.drawString(String.valueOf(game.curPlayerScore()), 1200, 300);
+        for (int i=0;i<4;i++){
+            add(fourButtonTiles[i]);
+            add(fourButtonAnimal[i]);
+            g.drawImage(tiles4[i], getWidth()/75, getHeight()/8+i*95, 75, 87, null);
+            g.drawImage(outline,   getWidth()/75, getHeight()/8+i*95, 75, 87, null);
+            fourButtonTiles[i].setBounds(getWidth()/75, getHeight()/8+i*95, 75, 87);
+            if (i == numSelectedTile){
+                g.drawImage(selectOutline, getWidth()/75, getHeight()/8+i*95, 75, 87, null);
+            }
+            if (i==numSelectedAnimal&&drawHighlightAnimal) {
+                g.drawImage(animalTokenMap.get(game.getAnimalToken4()[i])[1], 115, getHeight()/8+i*95, 60, 60, null);
+
+            }else{
+                g.drawImage(animalTokenMap.get(game.getAnimalToken4()[i])[0], 115, getHeight()/8+i*95, 60, 60, null);
+            }
+            fourButtonAnimal[i].setBounds(115, getHeight()/8+i*95, 60, 60);
+        }
     }
 
     public int getState(){
