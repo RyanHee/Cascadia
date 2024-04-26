261222e447dd90bf9dea7973ac841672d4e25903
Josh Owoeye <owoeyejay@gmail.com>
2024-04-26 12:15:47 -0500

    WAAIIIITTTTTTTTT

--- src/Panel.java
+++ src/Panel.java
@@ -357,7 +357,7 @@ public class Panel extends JPanel implements ActionListener {
             clearAnimals.setVisible(false);
             mixMatch.setVisible(false);
         try{
-            Thread.sleep(5);
+            wait(5);
         }
         catch (Exception E){
 
@@ -365,7 +365,7 @@ public class Panel extends JPanel implements ActionListener {
         repaint();
         }else if (prog==101){
             try{
-                Thread.sleep(250);
+                wait(250);
                 prog++;
                 nextTurn();
             }
@@ -776,5 +776,13 @@ public class Panel extends JPanel implements ActionListener {
     }
     public void resetProg(){
         prog = 0;
+    }
+    public void wait(int x){
+        try{
+            Thread.sleep(x);
+        }  
+        catch (Exception E){
+
+        }
     }
 }
