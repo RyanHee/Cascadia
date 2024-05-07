a496c74cc181012aaac44ee8b4424f9f3287f07b
Ryan <heyuanboryan814@outlook.com>
2024-05-07 00:46:32 -0500

    made progress bar faster

--- src/Frame.java
+++ src/Frame.java
@@ -11,17 +11,17 @@ public class Frame {
         JFrame frame = new JFrame("CASCADIA");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        //setSize(1600, 900);        
+        //setSize(1600, 900);
         JPanel contentPane = new JPanel();
         panel1 = new Menu3D(contentPane);
         //panel2 = new Panel(contentPane, Menu3D.playerCount);
         contentPane.setLayout(new CardLayout());
         frame.setVisible(true);
 
-        contentPane.add(panel1, "Panel 1"); 
+        contentPane.add(panel1, "Panel 1");
         //contentPane.add(panel2, "Panel 2");
         frame.setContentPane(contentPane);
-        
+
     }
     public static void main (String[] args) {
         new Frame().runner();
